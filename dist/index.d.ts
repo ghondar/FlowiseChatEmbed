81741310c9422d5433df8940f0cbf4de799bfe02
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2024-10-16 11:26:21 -0500

    [ UPDATE ] - sync last changes - fix full type props

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export type { BotProps } from './components/Bot';
 export type { BubbleProps } from './features/bubble/components/Bubble';
+export type { FullProps } from './features/full/components/Full';
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export type { BotProps } from './components/Bot';
 export type { BubbleProps } from './features/bubble/components/Bubble';
+export type { FullProps } from './features/full/components/Full';
